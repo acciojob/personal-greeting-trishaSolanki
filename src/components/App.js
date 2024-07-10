db47253b3281a2a1139f00057daabab3c19ca9ee
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-07-10 07:35:50 +0000

    x

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,19 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const[name,setName] = useState('');
+  const [name,setName]=useState("")
+
+  function handleChange(e){
+    setName("Hello "+ e.target.value+ "!");
+  }
+
   return (
-    <div>
-        <label>Enter your name :</label>
-      <input type='text' value={name} onChange={(e)=>setName('Hello' + e.target.value + '!')}/>
-       <p> {name}</p>
+<div>
+        {/* Do not remove the main div */}
+        <label>Enter your name: </label>
+        <input type="name" placeholder="name"onChange={handleChange}/>
+        <p>{name}</p>
     </div>
   )
 }
-
 export default App;
